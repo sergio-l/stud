0bf652ecafee85ebd0ad16d0942efcc7341ca25f
sergio-l <lytvyn.sergiy@gmail.com>
2014-11-15 20:12:29 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,6 +4,6 @@ require_once(__DIR__.'/../framework/Loader.php');
 
 Loader::addNamespacePath('Blog\\',__DIR__.'/../src/Blog');
 
-$app = new \Framework\Application(__DIR__.'/../app/config/config.php');
+//$app = new \Framework\Application(__DIR__.'/../app/config/config.php');
 
-$app->run();
+//$app->run();
