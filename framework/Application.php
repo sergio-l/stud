b9050af99a304e563e145a213a47d501d646e27d
sergio-l <lytvyn.sergiy@gmail.com>
2014-11-15 16:16:38 +0200

    Update Application.php

--- framework/Application.php
+++ framework/Application.php
@@ -6,26 +6,30 @@ use Framework\DI\Registry;
 use Framework\Router\Router;
 use Framework\Request\Request;
 
+/* Main class Application
+ *
+ */
+
 final class Application
 {
 
+    /*Installation configuration for application
+     *@param string $config
+     */
     public function __construct($config)
     {
-       $config = include($config);
-       Registry::set('config',$config);
-       Registry::set('routes',$config['routes']);
+        $config = include($config);
+        Registry::set('config',$config);
+        Registry::set('routes',$config['routes']);
 
 
     }
 
+    /*
+     * Run application
+     * @return void
+     */
     public function run()
-    {
-        $r = new Router(Request::getURI());
-        $r->findRoute();
-
-    }
-
-    public function getController($controller, $action, $params)
     {
 
     }
